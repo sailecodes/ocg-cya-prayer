be6d21c51e0c6208e25c1ca4227735d38f22e0a6
saile <eliasiv.codes@gmail.com>
2024-02-25 17:06:57 -0800

    first commit

---
+++ client/src/assets/components/pages/Layout.jsx
@@ -0,0 +1,48 @@
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const LayoutWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  height: 100vh;
+
+  .layout--header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 10rem;
+
+    padding: 2rem;
+    border-bottom: 1px solid var(--color-border);
+  }
+
+  .layout--header > p {
+    color: var(--color-primary);
+
+    font-size: 2.8rem;
+    font-weight: 600;
+    letter-spacing: 2px;
+    text-align: center;
+  }
+
+  .layout--footer {
+    height: 16rem;
+  }
+`;
+
+const Layout = () => {
+  return (
+    <LayoutWrapper>
+      <header className="layout--header">
+        <p>OCGrace College & Young Adults</p>
+      </header>
+      <Outlet />
+      <footer className="layout--footer"></footer>
+    </LayoutWrapper>
+  );
+};
+
+export default Layout;
---
+++ client/src/assets/components/pages/Main.jsx
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+const MainWrapper = styled.section`
+  display: grid;
+  place-items: center;
+
+  height: 100%;
+
+  padding: 4rem 2rem;
+
+  .main--tag {
+    color: var(--color-primary);
+
+    font-size: var(--font-text-5);
+    text-align: center;
+
+    margin-bottom: 2rem;
+  }
+
+  .main--tag span {
+    display: inline-block;
+
+    margin-top: 1rem;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  form > input {
+    color: var(--color-primary);
+
+    width: 28rem;
+    height: 4rem;
+
+    font-size: 1.8rem;
+
+    padding: 0 1rem;
+    margin-bottom: 2rem;
+    border: 1px solid var(--color-border);
+  }
+
+  form > button {
+    color: var(--color-white);
+    background-color: var(--color-primary);
+
+    height: 4rem;
+
+    width: 28rem;
+
+    font-size: 1.8rem;
+  }
+`;
+
+const Main = () => {
+  return (
+    <MainWrapper>
+      <div className="main--center-container">
+        <p className="main--tag">
+          If you have any silent requests, please let us know below! <br />
+          <span>We want to keep you in our prayers.</span>
+        </p>
+        <form>
+          <input type="text" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </MainWrapper>
+  );
+};
+export default Main;
